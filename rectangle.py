9c7210507a0cb9027d4eb18923936d83b21a453e
varalakshmisarathi <37236117+varalakshmisarathi@users.noreply.github.com>
2020-02-15 07:23:19 -0800

    Create rectangle.py

---
+++ rectangle.py
@@ -0,0 +1,11 @@
+#find area of a rectangle
+def rectangle():
+#get value of a
+ a=float(input())
+#get value of b
+ b=float(input())
+ area=a*b
+ return area
+res=rectangle()
+print(res)
+print(round(res,1))

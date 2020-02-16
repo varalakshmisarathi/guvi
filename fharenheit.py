d0100f666de3910b74c924f9c6e4dc79ed031d07
varalakshmisarathi <37236117+varalakshmisarathi@users.noreply.github.com>
2020-02-15 22:39:45 -0800

    Create fharenheit.py

---
+++ fharenheit.py
@@ -0,0 +1,10 @@
+#convert celsius into fahrenheit
+def fahrenheit():
+#get input
+ celsius = float(input(""))
+ #calculate fahrenheit
+ fahrenheit = (celsius * 9/5) + 32
+ return fahrenheit
+res=fahrenheit()
+print(res)
+

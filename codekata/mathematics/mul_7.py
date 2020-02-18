cbc7dd3d1f9f8ddc7d5c6d8155b7634aea043b6a
varalakshmisarathi <37236117+varalakshmisarathi@users.noreply.github.com>
2020-02-18 02:19:16 -0800

    Create mul_7.py

---
+++ codekata/mathematics/mul_7.py
@@ -0,0 +1,12 @@
+#find multiple of 7
+def multiple():
+#get input from user
+     n=int(input())
+     return n
+#call function
+res=multiple()
+#check whether the given number is multiple of 7
+if(res%7==0):
+     print("yes")
+else:
+     print("no")

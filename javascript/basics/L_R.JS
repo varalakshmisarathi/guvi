69a191fa94af77a50c630779723130bd307fb988
varalakshmisarathi <37236117+varalakshmisarathi@users.noreply.github.com>
2020-03-11 09:08:08 -0700

    Create L_R.JS

---
+++ javascript/basics/L_R.JS
@@ -0,0 +1,18 @@
+const readline=require('readline');
+const inp=readline.createInterface({input:process.stdin});
+const n=[]
+inp.on("line",(data)=>{
+    n.push(data);
+});
+inp.on("close",()=>{
+   var a=parseInt(n[0])
+   var b=n[1].split(' ')
+   var L=parseInt(b[0])
+   var R=parseInt(b[1])
+   if((a>=L)&&(a<=R)){
+       console.log('yes');
+   }
+   else{
+       console.log("no");
+   }
+});

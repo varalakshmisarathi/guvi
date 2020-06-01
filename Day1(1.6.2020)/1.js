3b7c7353879492dc3939210e43aad8985b53a595
varalakshmisarathi <37236117+varalakshmisarathi@users.noreply.github.com>
2020-06-01 18:57:45 +0000

    Create 1.js

---
+++ Day1(1.6.2020)/1.js
@@ -0,0 +1,60 @@
+//javascript object creation
+
+function objectcreation(){
+var TechnicalSpecifications={
+     Dimensions: {
+       overall length:3500,
+       overall width:1600,
+       overall height:1490,
+       Wheel base:2360,
+     trackwidth:{
+         
+       front:1405,
+       rear:1400
+                },
+       minimum turnin gradius:4.5,
+       minimum ground clearance:170,
+    
+    },
+    
+     capacities:{
+       seating capacity:5,
+       fueltank capacity:35
+     },
+     
+     Engine:{
+       type:"kB-series",
+       number of cylinders:3,
+       number of valves:12,
+       capacity:998,
+       bore x scrole:73.0*79.5,
+       compression ratio:10.1,
+       maximum power:67/6.200,
+       maximum torque:90/3.500,
+       fuel distribution:"multipoint injection"
+     },
+     transmission:{
+       type:"5-speed MT"
+     },
+     chassis:{
+         steering:"rack & pinion,power assisted",
+         brakes:{
+             front:"ventilated discs",
+             rear:"drums"
+         },
+         suspension:{
+             front:"MacPherson strut & coil spring",
+             rear:"isolated trailling link & coil spring"
+         },
+     
+        shock absorbers:"gas filled",
+        tyre(tubeless):"155/8OR13"
+     },
+     weights:{
+           kerb weight (min. with full options)kg:860-880,
+           cross vechile weight kg:1.320
+     }
+    
+}
+}
+objectcreation();

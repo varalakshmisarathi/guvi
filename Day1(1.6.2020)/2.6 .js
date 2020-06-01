baafbf6f30b9a853adbfe58d2ad07c7c0be34081
varalakshmisarathi <37236117+varalakshmisarathi@users.noreply.github.com>
2020-06-02 00:30:32 +0530

    Add files via upload

---
+++ Day1(1.6.2020)/2.6 .js
@@ -0,0 +1,40 @@
+//Print the price of Javascript book in console.
+function price(){
+var library=[
+    {
+        title:"javascript",
+        price:500,
+        readers:[
+            {
+                name:"person 1",
+                count:300
+            },
+            {
+                name:"person 2",
+                count:400
+            }
+            ],
+            authordetails:{
+                name:"raj",
+                age:40
+            },
+    },
+    {
+        title:"node js",
+        price:600,
+        readers:[],
+         authordetails:{
+                name:"raj",
+                age:40
+            }
+    }
+    
+    ]
+     //price of javascript book
+      var price=library[0].price;
+      console.log(price); 
+    //it print the price as 500
+    
+}
+price();
+    

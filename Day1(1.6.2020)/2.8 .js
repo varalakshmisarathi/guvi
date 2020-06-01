3550c2b40fbdab8bd7bd48ffe3558eda23230c7a
varalakshmisarathi <37236117+varalakshmisarathi@users.noreply.github.com>
2020-06-02 00:31:21 +0530

    Add files via upload

---
+++ Day1(1.6.2020)/2.8 .js
@@ -0,0 +1,48 @@
+//Print how many readers for javascript in console.
+function readers(){
+var library=[
+    {
+        title:"javascript",
+        price:500,
+        readers:[
+            {
+                name:"person 1",
+                count:300
+            },
+            {
+                name:"person 2",
+                count:400
+            }
+            ],
+            authordetails:{
+                name:"raj",
+                age:40
+            },
+    },
+    {
+        title:"node js",
+        price:600,
+        readers:[],
+         authordetails:{
+                name:"raj",
+                age:40
+            }
+    }
+    
+    ]
+     //assign count for counting the reader in javascript
+      var count = 0;
+ 
+      for (var i=0;i<library[0].readers.length;i++) 
+     {
+   
+      count = count + 1;
+  
+     } 
+     
+console.log(count);
+    //it print the count of reader in javascript
+    
+}
+readers();
+    

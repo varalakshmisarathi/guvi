2f851b0047bf1dc78e6a26af07da1d6227e32f0b
varalakshmisarathi <37236117+varalakshmisarathi@users.noreply.github.com>
2020-06-02 23:27:16 +0530

    Add files via upload

---
+++ Day2(2.6.2020)/API task/1.4.js
@@ -0,0 +1,11 @@
+//Display the count of all countries sharing their border with the country name in console
+var request=new XMLHttpRequest();
+request.open('GET','https://restcountries.eu/rest/v2/all',true)
+request.onload=function(){
+    var jsondata=JSON.parse(this.response);
+    for(i=0;i<jsondata.length;i++)
+    {
+        console.log(jsondata[i].flag);
+    }
+}
+request.send();

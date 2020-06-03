690cce74537644c6d9a6f43346398f9a46bf60e5
varalakshmisarathi <37236117+varalakshmisarathi@users.noreply.github.com>
2020-06-03 09:07:27 +0000

    Update problem5.js

--- Day2(2.6.2020)/JSON task/Problem 0 : Part B/problem5.js
+++ Day2(2.6.2020)/JSON task/Problem 0 : Part B/problem5.js
@@ -1,21 +1,13 @@
-/* Write a function “fromListToObject” which takes in an array of arrays, and returns an object with each pair of elements in the array as a key-value pair.
-Input (Array):
-var array = [[“make”, “Ford”], [“model”, “Mustang”], [“year”, 1964]];
-Output:
-var object = {
-make : “Ford”
-model : “Mustang”,
-year : 1964
-}*/
-
+// Write a function â€œfromListToObjectâ€ which takes in an array of arrays, and returns an object with each pair of elements in the array as a key-value pair.
 var arr = [['make', 'Ford'], ['model', 'Mustang'], ['year', 1964]];
 function fromListToObject(arr) {
- var Object = {};
+ var empty = {};
  for(var i=0; i<arr.length;i++){
  	var value=arr[i];
-    Object[value[0]]=value[1];
+    empty[value[0]]=value[1];
  }
  
- return Object;
+ return empty;
 }
+//call the function
 fromListToObject(arr);

63da9b340de7fa407c9814a92d50e0b13d5f50f2
varalakshmisarathi <37236117+varalakshmisarathi@users.noreply.github.com>
2020-06-03 08:36:33 +0530

    Add files via upload

---
+++ Day2(2.6.2020)/JSON task/Problem 0 : Part B/problem6.js
@@ -0,0 +1,26 @@
+/* Write a function called “transformGeekData” that transforms some set of data from one format to another.
+Input (Array):
+var array = [[[“firstName”, “Vasanth”], [“lastName”, “Raja”], [“age”, 24], [“role”, “JSWizard”]], [[“firstName”, “Sri”], [“lastName”, “Devi”], [“age”, 28], [“role”, “Coder”]]];
+Output:
+[
+{firstName: “Vasanth”, lastName: “Raja”, age: 24, role: “JSWizard”},
+{firstName: “Sri”, lastName: “Devi”, age: 28, role: “Coder”}
+] */
+
+var arr= [[['firstName', 'Vasanth'], ['lastName', 'Raja'], ['age', 24], ['role', 'JSWizard']], [['firstName', 'Sri'], ['lastName', 'Devi'], ['age', 28], ['role', 'Coder']]];
+function transformEmployeeData(arr) {
+ var array = [];
+ var empty={};
+ //iterate values
+ for(var i in arr){
+ 	var value=arr[i];
+   for(var j in value){
+   	empty[value[j][0]]=value[j][1];
+   }
+   List.push(empty);
+   empty={};
+ }
+ 
+ return array;
+}
+transformEmployeeData(arr);

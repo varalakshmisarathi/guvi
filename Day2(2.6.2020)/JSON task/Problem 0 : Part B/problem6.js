f56224a2bfdc3f53540f224afac02f90722fd39c
varalakshmisarathi <37236117+varalakshmisarathi@users.noreply.github.com>
2020-06-03 09:04:07 +0000

    Update problem6.js

--- Day2(2.6.2020)/JSON task/Problem 0 : Part B/problem6.js
+++ Day2(2.6.2020)/JSON task/Problem 0 : Part B/problem6.js
@@ -1,12 +1,4 @@
-/* Write a function called “transformGeekData” that transforms some set of data from one format to another.
-Input (Array):
-var array = [[[“firstName”, “Vasanth”], [“lastName”, “Raja”], [“age”, 24], [“role”, “JSWizard”]], [[“firstName”, “Sri”], [“lastName”, “Devi”], [“age”, 28], [“role”, “Coder”]]];
-Output:
-[
-{firstName: “Vasanth”, lastName: “Raja”, age: 24, role: “JSWizard”},
-{firstName: “Sri”, lastName: “Devi”, age: 28, role: “Coder”}
-] */
-
+//Write a function called â€œtransformGeekDataâ€ that transforms some set of data from one format to another.
 var arr= [[['firstName', 'Vasanth'], ['lastName', 'Raja'], ['age', 24], ['role', 'JSWizard']], [['firstName', 'Sri'], ['lastName', 'Devi'], ['age', 28], ['role', 'Coder']]];
 function transformEmployeeData(arr) {
  var array = [];

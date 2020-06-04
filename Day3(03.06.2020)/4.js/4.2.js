fbb4a7da26072d491ab33f024ebf3de26b846726
varalakshmisarathi <37236117+varalakshmisarathi@users.noreply.github.com>
2020-06-04 02:51:47 +0000

    Create 4.2.js

---
+++ Day3(03.06.2020)/4.js/4.2.js
@@ -0,0 +1,5 @@
+//Convert all the strings to title caps in a string array
+var animals=["cat","rat","cow","monkey"];
+var arr=a.map(item)=>{return
+item[0].toUpperCase()+item.substr(1).toLowercase()};
+console.log(arr);

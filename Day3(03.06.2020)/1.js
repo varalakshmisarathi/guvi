4a684a1c5c038b5c33379a6fe828a30f70bbb8e5
varalakshmisarathi <37236117+varalakshmisarathi@users.noreply.github.com>
2020-06-04 02:46:44 +0000

    Update 1.js

--- Day3(03.06.2020)/1.js
+++ Day3(03.06.2020)/1.js
@@ -1,3 +1,4 @@
+problem1
 //Write a function called “addFive”.
 //Given a number, “addFive” returns 5 added to that number.
 Input:
@@ -13,7 +14,7 @@ var result = addFive(num);
 console.log(result);
 
 
-
+problem2
 //Write a function called “getOpposite”.
 //Given a number, return its opposite
 Input:
@@ -40,7 +41,7 @@ var result = getOpposite(num)
 console.log(result);
 
 
-
+problem3
 //Fill in your code that takes an number minutes and converts it to seconds.
 //Examples
     toSeconds(5) ➞ 300
@@ -55,7 +56,7 @@ console.log(secs);
 
 
 
-
+problem4
 //Create a function that takes a string and returns it as an integer.
 //Examples
      toInteger(“6”) ➞ 6
@@ -69,7 +70,7 @@ var myint = toInteger(mystr)
 console.log(mystr);
 
 
-
+problem5
 //Create a function that takes a number as an argument, increments the number by +1 and returns the result.
 //Examples
     nextNumber(0) ➞ 1
@@ -84,7 +85,7 @@ console.log(myNextint);
 
 
 
-
+problem6
 //Create a function that takes an array and returns the first element.
 //Examples
    getFirstElement([1, 2, 3]) ➞ 1
@@ -96,7 +97,7 @@ return arr[0]
 }
 
 
-
+problem7
 //Convert Hours into Seconds
 //Write a function that converts hours into seconds.
 Examples
@@ -112,7 +113,7 @@ var data = hourToSeconds(arr)
 console.log(data);
 
 
-
+problem8
 //Find the Perimeter of a Rectangle
 //Create a function that takes height and width and finds the perimeter of a rectangle.
 Examples
@@ -127,7 +128,7 @@ console.log(peri);
 
 
 
-
+problem9
 //Less Than 100?
 //Given two numbers, return true if the sum of both numbers is less than 100. Otherwise return false.
 Examples
@@ -149,7 +150,7 @@ var res = lessThan100(22,15)
 console.log(res);
 
 
-
+problem10
 //There is a single operator in JavaScript, capable of providing the remainder of a division operation. Two numbers are passed as parameters. The first parameter divided by the second parameter will have a remainder, possibly zero. Return that value.
 //Examples
     remainder(1, 3) ➞ 1
@@ -163,7 +164,7 @@ var res = remainder(1,3)
 console.log(res);
 
 
-
+problem11
 //Old macdonald had a farm:
 //MacDonald is asking you to tell him how many legs can be counted among all his animals. The farmer breeds three species:
      turkey = 2 legs
@@ -183,7 +184,7 @@ console.log(legs);
 
 
 
-
+problem12
 //Frames Per Second
 //Create a function that returns the number of frames shown in a given number of minutes for a certain FPS.
 //Examples
@@ -199,7 +200,7 @@ console.log(fps);
 
 
 
-
+problem13
 //Check if an Integer is Divisible By Five
 //Create a function that returns true if an integer is evenly divisible by 5, and false otherwise.
 //Examples
@@ -220,7 +221,7 @@ var divisible = divisibleByFive(5)
 console.log(divisible);
 
 
-
+problem14
 //Write a function called “isEven”.
 //Given a number, “isEven” returns whether it is even.
 Input:
@@ -255,7 +256,7 @@ console.log(even);
 
 
 
-
+problem15
 //Write a function called “areBothOdd”.
 //Given 2 numbers, “areBothOdd” returns whether or not both of the given numbers are odd.
 Input:
@@ -279,3 +280,131 @@ function areBothOdd(num1, num2){
 }
 var odd=areBothOdd(1,3)
 console.log(odd);
+
+
+
+problem16
+//Write a function called “getFullName”.
+//Given a first and a last name, “getFullName” returns a single string with the given first and last names separated by a single space.
+Input:
+    getFullName(“GUVI”, “GEEK”);
+    getFullName(“GUVI”, );
+    getFullName(, “GEEK”);
+    getFullName(“”, “”);
+Output:
+    “GUVI GEEK”
+    “GUVI”
+    “GEEK”
+     “”
+function getFullName(firstName, lastName){
+ return firstName+lastName
+}
+var name=getFullName("", "GEEK");
+console.log(name);
+
+
+
+//problem17
+//Write a function called “getLengthOfWord”.
+//Given a word, “getLengthOfWord” returns the length of the given word.
+Input:
+    getLengthOfWord(“GUVI”);
+    getLengthOfWord(“”);
+    getLengthOfWord();
+    getLengthOfWord(9);
+Output:
+    4
+    0
+    -1
+    -1
+function getLengthOfWord(word1){
+if(typeof(word1)=="String")
+ {
+ return word1.length
+ }
+ else
+ {
+ return -1
+ }
+ }
+ var res=getLengthOfWord(word1)
+ console.log(res);
+
+
+
+//problem18
+//Write a function called “isSameLength”.
+//Given two words, “isSameLength” returns whether the given words have the same length.
+Input:
+   isSameLength(“GUVI”, “GEEK”);
+Output:
+    true
+function isSameLength(word1, word2){
+ var str1=word1.length;
+ var str2=word2.length;
+ if(str1==str2)
+ {
+ return true
+ }
+ else
+ {
+ return false
+ }
+}
+var result= isSameLength(word1, word2)
+console.log(result);
+
+
+
+//problem18
+//Create a function to calculate the distance between two points defined by their x, y coordinates
+function getDistance(x1, y1, x2, y2)
+{
+ var sum1=x1+x2;
+ var sum2=y1+y2;
+ return(sum1-sum2);
+}
+console.log(getDistance(100, 100, 400, 300));
+
+
+
+//problem19
+//Write a function called “getNthElement”.
+//Given an array and an integer, “getNthElement” returns the element at the given integer, within the given array. If the array has a length of 0, it should return ‘undefined’.
+Input:
+  getNthElement([1, 3, 5], 1);
+Output:
+   3
+function getNthElement(array,n){
+
+for(i=0;i<array.length;i++)
+ {
+ if(array[i]==n)
+ {
+ console.log(array[n]);
+ }
+ 
+ else if(array.length==0)
+ {
+ console.log("undefined");
+ }
+}}
+
+
+
+//problem20
+//Write a function called “getLastElement”.
+//Given an array, “getLastElement” returns the last element of the given array. If the given array has a length of 0, it should return ‘-1’.
+Input:
+   getLastElement([1, 2, 3, 4]);
+Output:
+    4
+function getLastElement(array){
+ if(array.length===0){
+ 	return -1
+ }
+ else{
+ 	return array[array.length-1]
+ }
+}
+ console.log(getLastElement([1, 2, 3, 4]));

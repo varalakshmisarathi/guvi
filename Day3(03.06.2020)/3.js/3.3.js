f90e4c734846f2829f9c48ece9bc4b94df008e44
varalakshmisarathi <37236117+varalakshmisarathi@users.noreply.github.com>
2020-06-04 12:21:07 +0000

    Create 3.3.js

---
+++ Day3(03.06.2020)/3.js/3.3.js
@@ -0,0 +1,19 @@
+//if the number is palindrome
+var number=101;
+function function1(n)
+var a='mom';
+var s='';
+for(i=(a.length)-1;i>=0;i--)
+{
+s+=a[i]+'';
+}
+if(s==a)
+{
+    return("palindrome");
+}
+
+function function2(res)
+{
+console.log(res);
+}
+function2(function1)

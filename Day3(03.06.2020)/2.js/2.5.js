6c20f928f0ef33894f549ea2ad32563416f82889
varalakshmisarathi <37236117+varalakshmisarathi@users.noreply.github.com>
2020-06-04 12:09:04 +0000

    Create 2.5.js

---
+++ Day3(03.06.2020)/2.js/2.5.js
@@ -0,0 +1,12 @@
+//turn all the palindromes in an array
+var string=["animal","madam","mam"]
+var palindrome = function ()
+{
+var palin=str.filter(function(z){
+      var b=z.split('').reverse().join('') 
+      if(z==b){
+          console.log(z)
+      }
+}) 
+};
+palindrome()

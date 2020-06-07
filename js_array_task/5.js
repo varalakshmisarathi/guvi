59cc0d2b54c4bb5e9799336170001530d2d80a77
varalakshmisarathi <37236117+varalakshmisarathi@users.noreply.github.com>
2020-06-07 12:25:35 +0000

    Create 5.js

---
+++ js_array_task/5.js
@@ -0,0 +1,9 @@
+//5 - dropRight
+//Creates a slice of array with n elements dropped from the end.
+function drop(array)
+{
+var result=array.slice(0,2);
+console.log(result);
+}
+drop([1,  2,  3]);
+

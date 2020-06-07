d4d8a30b8d118d6396c0dedaaa4aeda476ab4d5a
varalakshmisarathi <37236117+varalakshmisarathi@users.noreply.github.com>
2020-06-07 12:31:20 +0000

    Create 7.js

---
+++ js_array_task/7.js
@@ -0,0 +1,8 @@
+//7 - findLastIndex
+//This method is like Array.prototype.findIndex except that it iterates over elements of collection from right to left.
+function index(array)
+{
+var result = array.lastIndexOf(8);
+console.log(result);
+}
+index([5, 12, 6, 130, 8]);
